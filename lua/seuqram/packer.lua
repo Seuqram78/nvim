49f8d433f5b45b97a9543edb781509d591723f59
Rodrigo Marques <rodrigo.marques@visagio.com>
2025-05-25 16:39:00 +0800

    adding lsp

--- lua/seuqram/packer.lua
+++ lua/seuqram/packer.lua
@@ -21,4 +21,14 @@ return require('packer').startup(function(use)
 
   use ('mbbill/undotree')
 
+  use ('neovim/nvim-lspconfig', {tag = 'v1.8.0', pin = true})
+
+  use('hrsh7th/cmp-nvim-lsp')
+  
+  use('hrsh7th/nvim-cmp')
+
+  use('mason-org/mason.nvim', {tag = 'v1.11.0', pin = true})
+  
+  use('mason-org/mason-lspconfig.nvim', {tag = 'v1.32.0', pin = true})
+
 end)
