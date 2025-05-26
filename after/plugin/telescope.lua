581ce70a6c8923a3f5882e6b63f617e904e08615
Rodrigo Marques <rodrigo.marques@visagio.com>
2025-05-26 18:06:30 +0800

    adjusting grep shortcut

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>aa', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>ar', builtin.git_files, { desc = 'Find in a git repository' })
+vim.keymap.set('n', '<leader>as', builtin.live_grep, { desc = "Live Grep" })
 vim.keymap.set('n', '<leader>tla', ':Telescope diagnostics<CR>')
-vim.keymap.set('n', '<leader>as', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
 vim.api.nvim_set_keymap('n', '<leader>tll', '', {
     noremap = true,
     silent = true,
