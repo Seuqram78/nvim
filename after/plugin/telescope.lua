4e6904c39f53ad90f62cc82a4e4b7a333e611b47
Rodrigo Marques <rodrigo.marques@visagio.com>
2025-05-26 17:52:50 +0800

    updating git ignore to ignore the right folder

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,12 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>aa', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>ar', builtin.git_files, { desc = 'Find in a git repository' })
+vim.keymap.set('n', '<leader>tla', ':Telescope diagnostics<CR>')
 vim.keymap.set('n', '<leader>as', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.api.nvim_set_keymap('n', '<leader>tll', '', { noremap = true, silent = true, callback = function()
+    require('telescope.builtin').diagnostics({ bufnr = 0 })
+end }) 
 
 
--- lua/seuqram/packer.lua
+++ lua/seuqram/packer.lua
@@ -2,43 +2,43 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-  use('ThePrimeagen/harpoon')
+    use('ThePrimeagen/harpoon')
 
-  use('tpope/vim-fugitive')
+    use('tpope/vim-fugitive')
 
-  use ('mbbill/undotree')
+    use('mbbill/undotree')
 
-  use ('neovim/nvim-lspconfig', {tag = 'v1.8.0', pin = true})
+    use('neovim/nvim-lspconfig', { tag = 'v1.8.0', pin = true })
 
-  use('hrsh7th/cmp-nvim-lsp')
-  
-  use('hrsh7th/nvim-cmp')
+    use('hrsh7th/cmp-nvim-lsp')
 
-  use('mason-org/mason.nvim', {tag = 'v1.11.0', pin = true})
+    use('hrsh7th/nvim-cmp')
 
-  use('mason-org/mason-lspconfig.nvim', {tag = 'v1.32.0', pin = true})
+    use('mason-org/mason.nvim', { tag = 'v1.11.0', pin = true })
 
-  use({
-      "kdheepak/lazygit.nvim",
-  })
+    use('mason-org/mason-lspconfig.nvim', { tag = 'v1.32.0', pin = true })
 
-  use ("akinsho/toggleterm.nvim", {tag = '*'})
+    use({
+        "kdheepak/lazygit.nvim",
+    })
 
-  use (
-      "WhoIsSethDaniel/mason-tool-installer.nvim", 
-      { requires = "williamboman/mason.nvim" }
-  )
+    use("akinsho/toggleterm.nvim", { tag = '*' })
+
+    use(
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        { requires = "williamboman/mason.nvim" }
+    )
 end)
