7ca411c6d2dff12eb5b9e816ec5aec62ba38ddf9
Rodrigo Marques <rodrigo.marques@protonmail.com>
2025-08-30 22:55:25 +0800

    updating plugins, settings and keymaps

--- lua/03_settings.lua
+++ lua/03_settings.lua
@@ -1,26 +1,26 @@
 require("nvim-treesitter.configs").setup({
-	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
 
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
 
-	-- List of parsers to ignore installing (or "all")
-	ignore_install = { "javascript" },
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { "javascript" },
 
-	highlight = {
-		enable = true,
+  highlight = {
+    enable = true,
 
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
 })
 
 vim.o.background = "dark" -- or "light" for light mode
@@ -29,133 +29,144 @@ vim.cmd([[colorscheme gruvbox]])
 require("mini.files").setup()
 
 require("lualine").setup({
-	sections = {
-		lualine_c = { "filename", "lsp_status" },
-	},
-	options = { theme = "gruvbox" },
+  sections = {
+    lualine_c = { "filename", "lsp_status" },
+  },
+  options = { theme = "gruvbox" },
 })
+
+-- Harpoon
+require("harpoon"):setup()
 
 -- Telescope
 local ok, telescope = pcall(require, "telescope")
 local actions = require("telescope.actions")
 if ok then
-	telescope.setup({
-		defaults = {
-			file_ignore_patterns = { "node_modules", ".git/" },
-			mappings = {
-				i = {
-					["<ESC>"] = actions.close,
-				},
-			},
-		},
-		pickers = {
-			find_files = {
-				hidden = true, --Show hidden files (dotfiles)
-				no_ignore = true, -- Don't respect .gitignore
-				follow = true, -- Follow symlinks
-			},
-		},
-	})
+  telescope.setup({
+    defaults = {
+      file_ignore_patterns = { "node_modules", ".git/" },
+      mappings = {
+        i = {
+          ["<ESC>"] = actions.close,
+        },
+      },
+    },
+    pickers = {
+      find_files = {
+        hidden = true,    --Show hidden files (dotfiles)
+        no_ignore = true, -- Don't respect .gitignore
+        follow = true,    -- Follow symlinks
+      },
+    },
+  })
 end
 
 -- Conform
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = {
-			-- To fix auto-fixable lint errors.
-			"ruff_fix",
-			-- To run the Ruff formatter.
-			"ruff_format",
-			-- To organize the imports.
-			"ruff_organize_imports",
-		},
-		-- You can customize some of the format options for the filetype (:help conform.format)
-		rust = { "rustfmt", lsp_format = "fallback" },
-		-- Conform will run the first available formatter
-		javascript = { "prettierd", "prettier", stop_after_first = true },
-	},
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    python = {
+      -- To fix auto-fixable lint errors.
+      "ruff_fix",
+      -- To run the Ruff formatter.
+      "ruff_format",
+      -- To organize the imports.
+      "ruff_organize_imports",
+    },
+    -- You can customize some of the format options for the filetype (:help conform.format)
+    rust = { "rustfmt", lsp_format = "fallback" },
+    typescript = { "prettier" },
+    javascript = { "prettier" },
+    html = { "prettier" },
+  },
+  formatters = {
+    prettier = {
+      command = "prettier",
+      args = { "--stdin-filepath", "$FILENAME" },
+      stdin = true,
+    }
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 3000,
+    lsp_format = "fallback",
+  },
 })
 
 -- DAP
 local dap = require("dap")
 
 dap.adapters.python = {
-	type = "executable",
-	command = "python",
-	args = { "-m", "debugpy.adapter" },
+  type = "executable",
+  command = "python",
+  args = { "-m", "debugpy.adapter" },
 }
 
 dap.configurations.python = {
-	{
-		type = "python",
-		request = "launch",
-		name = "Launch file",
-		program = "${file}", -- This means: run current file
-		pythonPath = function()
-			return "python" -- Or set your venv path here
-		end,
-	},
+  {
+    type = "python",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}", -- This means: run current file
+    pythonPath = function()
+      return "python"    -- Or set your venv path here
+    end,
+  },
 }
 require("nvim-dap-virtual-text").setup({
-	-- optional settings
-	commented = true, -- show virtual text as comments
+  -- optional settings
+  commented = true, -- show virtual text as comments
 })
 
 -- CMP
 local cmp = require("cmp")
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<Tab>"] = cmp.mapping.select_next_item(),
-		["<S-Tab>"] = cmp.mapping.select_prev_item(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	}),
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-	},
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  },
 })
 
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 lspconfig.lua_ls.setup({
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			runtime = { version = "LuaJIT" },
-			diagnostics = {
-				globals = { "vim" }, -- Recognize `vim` as a global
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			telemetry = { enable = false },
-		},
-	},
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = {
+        globals = { "vim" }, -- Recognize `vim` as a global
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = { enable = false },
+    },
+  },
 })
 
-lspconfig.pyright.setup({
-	settings = {
-		python = {
-			analysis = {
-				autoImportCompletions = true,
-				autoSearchPaths = true,
-				diagnosticMode = "workspace",
-				useLibraryCodeForTypes = true,
-			},
-		},
-	},
-})
+-- lspconfig.pyright.setup({
+--   settings = {
+--     python = {
+--       analysis = {
+--         autoImportCompletions = true,
+--         autoSearchPaths = true,
+--         diagnosticMode = "workspace",
+--         useLibraryCodeForTypes = true,
+--       },
+--     },
+--   },
+-- })
--- lua/04_keymaps.lua
+++ lua/04_keymaps.lua
@@ -1,36 +1,76 @@
+-- Telescope
+local builtin = require("telescope.builtin")
+local input = vim.fn.input
+
 vim.keymap.set("n", "<leader>tf", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy Find Files" })
-vim.keymap.set("n", "<leader>ts", "<cmd>Telescope live_grep<CR>", { desc = "Fuzzy Search" })
+
+vim.keymap.set("n", "<leader>ts", function()
+  -- Prompt user for extensions
+  local extensions = input("Enter file extension: ")
+
+  -- If blank, search all files
+  if extensions == "" then
+    builtin.live_grep()
+    return
+  else
+    -- Build ripgrep --glob arguments
+    local args = {}
+    for extension in string.gmatch(extensions, '[^,]+') do
+      table.insert(args, "--glob=*" .. extension)
+    end
+
+    -- Run live_grep with extension filter
+    builtin.live_grep({ additional_args = args })
+  end
+end, { desc = "Fuzzy Search" })
+
 vim.keymap.set("n", "<leader>tr", "<cmd>Telescope buffers<CR>", { desc = "List Buffers" })
+vim.keymap.set("n", "<leader>nei", ':lua require("telescope.builtin).lsp_references()<CR>', { desc = "Find usages" })
+
+--Harpoon
+local harpoon = require("harpoon")
+vim.keymap.set("n", "<leader>hv", function() harpoon:list():add() end, { desc = "Harpoon add" })
+vim.keymap.set("n", "<leader>hk", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+  { desc = "Harpoon toggle" })
+vim.keymap.set("n", "<leader>hz", function() harpoon:list():select(1) end, { desc = "Harpoon 1" })
+vim.keymap.set("n", "<leader>hx", function() harpoon:list():select(2) end, { desc = "Harpoon 2" })
+vim.keymap.set("n", "<leader>hc", function() harpoon:list():select(3) end, { desc = "Harpoon 3" })
+vim.keymap.set("n", "<leader>hd", function() harpoon:list():select(4) end, { desc = "Harpoon 4" })
+vim.keymap.set("n", "<leader>h,", function() harpoon:list():prev() end, { desc = "Harpoon prev" })
+vim.keymap.set("n", "<leader>h.", function() harpoon:list():next() end, { desc = "Harpoon next" })
 
 -- mini files
 vim.keymap.set("n", "<leader>pv", function()
-	require("mini.files").open()
+  require("mini.files").open()
 end)
 
 -- DAP
 local dap = require("dap")
 vim.keymap.set("n", "<F5>", function()
-	dap.continue()
+  dap.continue()
 end, { desc = "DAP: Continue" })
 vim.keymap.set("n", "<F7>", function()
-	dap.step_into()
+  dap.step_into()
 end, { desc = "DAP: Step Into" })
 vim.keymap.set("n", "<F8>", function()
-	dap.step_over()
+  dap.step_over()
 end, { desc = "DAP: Step Over" })
 vim.keymap.set("n", "<F9>", function()
-	dap.continue()
+  dap.continue()
 end, { desc = "DAP: Resume" })
 vim.keymap.set("n", "<leader>bb", function()
-	dap.toggle_breakpoint()
+  dap.toggle_breakpoint()
 end, { desc = "DAP: Toggle Breakpoint" })
 
 -- DAP UI
 local dapui = require("dapui")
 
 vim.keymap.set("n", "<leader>dd", function()
-	dapui.open()
+  dapui.open()
 end, { desc = "DAP UI Open" })
 vim.keymap.set("n", "<leader>dc", function()
-	dapui.close()
+  dapui.close()
 end, { desc = "DAP UI Close" })
+
+-- Toggleterm
+vim.keymap.set("n", "<leader>hh", "<cmd>ToggleTerm direction=float<CR>", { desc = "Open/close floating terminal" })
